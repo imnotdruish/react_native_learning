3c77787d076325c6c66b1aaa66e1c0ea67111e44
imnotdruish <105814537+imnotdruish@users.noreply.github.com>
2024-12-01 20:27:30 -0500

    Halfway through basic Weather app

---
+++ WeatherApp/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import CurrentWeather from "./src/screens/CurrentWeather"
+import UpcomingWeather from './src/screens/UpcomingWeather'
+import City from './src/screens/City'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+const Tab = createBottomTabNavigator()
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name={'Current'} component={CurrentWeather} />
+        <Tab.Screen name={'Upcoming'} component={UpcomingWeather} />
+        <Tab.Screen name={'City'} component={City} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App
